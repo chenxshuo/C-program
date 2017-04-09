7666a1be33ae4e5e401f954393b872391d019e15
chenxshuo <c1094829085@qq.com>
2017-04-09 11:42:23 +0800

    create new file

---
+++ ex1.c
@@ -0,0 +1,9 @@
+/*The first program */
+#include <stdio.h>
+/* 上面的是预编译命令 被C预处理程序处理 辅助实现下面的printf的调用*/
+int main(void)
+{
+    printf("hello world\n");
+    printf("Welcome\nto\nC!!\n");
+	return 0;
+}
